a28f22920412786da7c354fc9a82596c9a3e2f5c
BMFirman <bfirman@fastmail.com>
2018-12-10 16:48:32 +1300

    Correctly formats and runs a specified playlist

--- JTunes.java
+++ JTunes.java
@@ -2,13 +2,19 @@ import java.lang.*;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Formatter;
+import java.util.FormatterClosedException;
+import java.nio.file.Paths;
 
 class JTunes {
     public static void main(String[] args) throws IOException {
         
 
         try {
-            List<String> list = new ArrayList<String>(); 
+            List<String> list = new ArrayList<String>();
+            list.add("osascript");
+            list.add("script.scpt");
+            /*
             list.add("osascript");
 
             if (args[0].equals("play")) {
@@ -19,6 +25,8 @@ class JTunes {
                 //playbackToggle("pause.scpt");
                 list.add("pause.scpt"); 
             }
+            */
+            playPlaylist(args[0]);
 
             ProcessBuilder build = new ProcessBuilder(list); 
             Process p = build.start();
@@ -46,4 +54,46 @@ class JTunes {
         }
     }
     */
+
+    static void playPlaylist(String playlistName) {
+        String output =
+        "tell application \"iTunes\"\n" +
+        "  play the playlist named \"" +
+        playlistName +
+        "\"\n" +
+        "end tell";
+
+        writeScript(output);
+    }
+
+    static void writeScript(String command) {
+
+        String filename = "script.scpt";
+        String workingDirectory = System.getProperty("user.dir");
+
+        Formatter output = null;
+        output = initFormatter(filename, workingDirectory, output);
+
+        output.format("-- JTunes auto script\n");
+        output.format(command);
+        
+
+        output.close();
+    }
+
+
+    public static Formatter initFormatter(String filename, String workingDirectory, Formatter output) {
+
+        try {
+            output = new Formatter(Paths.get(workingDirectory) + "/" + filename);
+        } catch (SecurityException secExc) {
+            System.err.println("Can't write to .scpt!");
+            System.exit(1);
+        } catch (FileNotFoundException fnfExc) {
+            System.err.println("Can't find path to .scpt!");
+            System.exit(1);
+        }
+
+        return output;
+    }
 }
---
+++ script.scpt
@@ -0,0 +1,4 @@
+-- JTunes auto script
+tell application "iTunes"
+  play the playlist named "Media"
+end tell
