dc9f31a1e9fb2f5af03024eb9dc3e1a8ede4bbd2
BMFirman <bfirman@fastmail.com>
2018-12-10 14:02:56 +1300

    Proof of concept, plays and pauses

--- JTunes.java
+++ JTunes.java
@@ -5,18 +5,31 @@ import java.util.List;
 
 class JTunes {
     public static void main(String[] args) throws IOException {
+        
+
         try {
-            if (args[0] == "Play") {
-                playbackToggle("play.scpt");
-            } else if (args[0] == "Pause") {
-                playbackToggle("pause.scpt");
+            List<String> list = new ArrayList<String>(); 
+            list.add("osascript");
+
+            if (args[0].equals("play")) {
+                //playbackToggle("play.scpt");
+                list.add("play.scpt"); 
+            } 
+            if (args[0].equals("pause")) {
+                //playbackToggle("pause.scpt");
+                list.add("pause.scpt"); 
             }
+
+            ProcessBuilder build = new ProcessBuilder(list); 
+            Process p = build.start();
+            System.out.println("command: " + build.command()); 
+
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("No flag was provided.");
         }
 
     }
-
+    /*
     static void playbackToggle(String playbackSelection){
         try {
             // creating list of process 
@@ -32,4 +45,5 @@ class JTunes {
             System.out.println("Failed to run osascript");
         }
     }
+    */
 }
